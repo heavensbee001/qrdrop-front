6c52778462ae3c415410b5084dbeb291477a15a8
heavensbee001 <heavensbee001@gmail.com>
2022-06-16 14:05:19 +0200

    Add createPOAP button

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+    const handleClickAddPoap = () => {};
     return (
         <div className={styles.container}>
             <Head>
@@ -13,6 +14,19 @@ const Home: NextPage = () => {
             </Head>
 
             <h1 className="">QRdrop</h1>
+            <section>
+                <div className="bg-orange relative w-48 h-12 box-border flex">
+                    <div className="h-full w-12 h-12 z-2 flex justify-center items-center text-white font-bold text-2xl">
+                        +
+                    </div>
+                    <div
+                        className="bg-white border-orange border-4 border-l-0 w-36 flex justify-center items-center roboto-font text-orange"
+                        onClick={handleClickAddPoap}
+                    >
+                        <span className="">Add POAP</span>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
